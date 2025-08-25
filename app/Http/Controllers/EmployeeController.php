9f7b1ef76b098b6267992dee6387d7fbed2945a9
SyedOmarAli <syedomarali.smit@gmail.com>
2025-08-25 10:48:55 -0700

    search filter updated

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -28,7 +28,10 @@ class EmployeeController extends Controller
                 $query->where(function ($q) use ($search) {
                     $q->where('name', 'like', '%' . $search . '%')
                         ->orWhere('email', 'like', '%' . $search . '%')
-                        ->orWhere('department', 'like', '%' . $search . '%');
+                        ->orWhere('department', 'like', '%' . $search . '%')
+                        ->orWhere('phone', 'like', '%' . $search . '%')
+                        ->orWhere('salary', 'like', '%' . $search . '%')
+                        ->orWhere('hire_date', 'like', '%' . $search . '%');
                 });
             }
 
